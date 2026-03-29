e9be5511abee2f72a5136e9530b5239aa0944c54
isaac-ndungu <isaacndungu478@gmail.com>
2026-03-29 16:14:53 +0300

    add utilities, AppError class for error throwing, catchAsych for controllers, and jwt functions

---
+++ server/src/utils/catchAsync.js
@@ -0,0 +1,9 @@
+// Wraps async controller functions 
+// passes errors to Express error handler
+const catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next);
+    };
+};
+
+export default catchAsync;
---
+++ server/src/utils/jwt.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+export const signAccessToken = (userId) => {
+    return jwt.sign(
+        { id: userId },
+        process.env.JWT_ACCESS_SECRET,
+        { expiresIn: process.env.JWT_ACCESS_EXPIRY }   // '15m'
+    );
+};
+
+export const signRefreshToken = (userId) => {
+    return jwt.sign(
+        { id: userId },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: process.env.JWT_REFRESH_EXPIRY }  // '7d'
+    );
+};
+
+export const verifyAccessToken = (token) => {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+
+export const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
+
+// Sends the refresh token as an httpOnly cookie
+export const attachRefreshTokenCookie = (res, token) => {
+  res.cookie('refreshToken', token, {
+    httpOnly: true,       //  XSS protection
+    secure: process.env.NODE_ENV === 'production',   
+    sameSite: 'strict',
+    maxAge: 7 * 24 * 60 * 60 * 1000   // 7 days 
+  });
+};
